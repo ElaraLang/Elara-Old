0d90675b3a58d6cc70deebbf4f00395bb3aacfae
Vaishnav <vaishnav.anil@rocketmail.com>
2020-08-12 18:04:28 +0530

    Added basic parser structure

---
+++ src/main/kotlin/io/github/elaralang/elara/parser/ASTNode.kt
@@ -0,0 +1,16 @@
+package io.github.elaralang.elara.parser
+
+sealed class ASTNode() {
+    private val children = mutableListOf<ASTNode>()
+    fun addChild(node: ASTNode) {
+        children.add(node)
+    }
+    fun removeChild(node: ASTNode) {
+        children.remove(node)
+    }
+}
+
+class RootNode: ASTNode()
+
+class AssignmentNode(val identifier: String,val value: ASTNode): ASTNode()
+
--- src/main/kotlin/io/github/elaralang/elara/parser/ElaraParser.kt
+++ src/main/kotlin/io/github/elaralang/elara/parser/ElaraParser.kt
@@ -1,10 +1,88 @@
 package io.github.elaralang.elara.parser
 
+import io.github.elaralang.elara.exceptions.invalidSyntax
+import io.github.elaralang.elara.lexer.ElaraLexer
 import io.github.elaralang.elara.lexer.Token
+import io.github.elaralang.elara.lexer.TokenType
+import java.util.*
 
-class ElaraParser {
+class ElaraParser(tokenList: List<Token>) {
 
-    fun parse(tokens: List<Token>) {
+    private val tokens = Stack<Token>()
+    init {
+        tokens.addAll(tokenList)
+        tokens.reverse()
+    }
 
+    fun parse(): RootNode {
+        val rootNode = RootNode()
+        while (tokens.isNotEmpty()) {
+            val child = parseExpression()
+            child?.let { rootNode.addChild(it) }
+
+        }
+        return rootNode
+    }
+
+
+    private fun parseExpression(lastToken: Token? = null): ASTNode? {
+        val currentToken = tokens.pop()
+
+        if (lastToken == null) {
+            //No prior context
+
+            when (currentToken.type) {
+                // let test = <expression>
+                TokenType.LET -> {
+                    return parseAssignment()
+                }
+                // ambiguous => requires next Token for context
+                TokenType.IDENTIFIER -> {
+                    return parseExpression(currentToken)
+                }
+            }
+
+        } else {
+            // Check last context
+
+            when (lastToken.type) {
+                TokenType.IDENTIFIER -> {
+                    when (currentToken.type) {
+                        TokenType.LPAREN -> {
+                            return parseFunctionCall(lastToken, TokenType.COMMA, TokenType.RPAREN)
+                        }
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+
+    private fun parseFunctionCall(identifier: Token, separator: TokenType, endtype: TokenType): ASTNode {
+        val params = parseParams(separator, endtype)
+        TODO("Parse the scope")
+    }
+
+    private fun parseParams(separator: TokenType, endtype: TokenType): ASTNode {
+        TODO("Parse params with separator such that it functions ")
+    }
+
+    private fun parseAssignment(): AssignmentNode {
+        val id = tokens.pop()
+
+        if (id.type != TokenType.IDENTIFIER) {
+            invalidSyntax("Identifier expected on assignment, found ${id.text} of type ${id.type}")
+        }
+
+        val eql = tokens.pop()
+
+        if (eql.type != TokenType.DEF) {
+            invalidSyntax("'=' expected on assignment, found ${id.text} of type ${id.type}")
+        }
+
+        val value = parseExpression() ?: invalidSyntax("Could not find expression to assign to ${id.text}")
+        return AssignmentNode(id.text, value)
     }
 }
+
